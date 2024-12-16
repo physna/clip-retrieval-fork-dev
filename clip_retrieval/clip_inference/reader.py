d314899a0a115265969df680a00ae737060d191f
Sean Wedig <sean@physna.com>
2024-12-16 10:32:04 -0500

    support reading sparse webdatasets that may have tasks that don't find files

--- clip_retrieval/clip_inference/reader.py
+++ clip_retrieval/clip_inference/reader.py
@@ -144,7 +144,9 @@ def create_webdataset(
 
     urls = input_sampler(urls)
 
-    dataset = wds.WebDataset(urls, cache_dir=cache_path, cache_size=10**10, handler=wds.handlers.warn_and_continue)
+    dataset = wds.WebDataset(
+        urls, cache_dir=cache_path, cache_size=10**10, handler=wds.handlers.warn_and_continue, empty_check=False
+    )
 
     def _tokenizer(text):
         return tokenizer([text])[0]
